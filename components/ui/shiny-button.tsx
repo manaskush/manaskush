30b7ea33b4f5afd4dde38eb9755beec6275e9d44
Manas Kush <130543405+manaskush@users.noreply.github.com>
2025-05-28 09:42:35 +0530

    Update shiny-button.tsx

--- components/ui/shiny-button.tsx
+++ components/ui/shiny-button.tsx
@@ -45,7 +45,7 @@ const ShinyButton = React.forwardRef<HTMLButtonElement, ShinyButtonProps>(
           "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
           className,
         )}
-      ><a href="https://drive.google.com/file/d/1RswxDJCg55qxQOTbH72udITdRYqiEwIE/view?usp=sharing">
+      ><a href="https://drive.google.com/file/d/1U_wTsZenHmYaCf4yQ-aJN18tQeyZJGle/view?usp=sharing">
         <span
           className="relative block size-full text-sm uppercase tracking-wide text-[rgb(0,0,0,65%)] dark:font-light dark:text-[rgb(255,255,255,90%)]"
           style={{
